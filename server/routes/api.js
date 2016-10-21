123881887154e7b2a1401563c8383a38e2a39fe0
Jason <jftcode@gmail.com>
2016-10-20 18:58:15 -0700

    (feat) Rough out server structure

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const router = express.Router();
+
+const router = express.Router(); // eslint-disable-line
+
+const handler = require('../handlers/apiHandler');
 
 router.get('/bill/:id', /* funct to handle bill*/);
-router.post('/bill', /* funct to create bill */);
+router.post('/bill', handler.saveBill);
 
 module.exports = router;
