5df6a26c56fbae8ea6ef1f3006dee08ad4394078
Luis Villeda <luisevilleda@gmail.com>
2016-10-20 20:00:22 -0700

    Implement ability to change bill Item values

--- client/src/BillItem.js
+++ client/src/BillItem.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './BillItem.css';
 
 class BillItem extends Component {
+  constructor(props) {
+    super(props);
+    this.descriptionChange = this.descriptionChange.bind(this);
+    this.priceChange = this.priceChange.bind(this);
+  }
+
+  descriptionChange(event) {
+    this.props.billItemChange(this.props.index, event.target.value, null);
+  }
+  priceChange(event) {
+    this.props.billItemChange(this.props.index, null, event.target.value);
+  }
+
   render() {
     return (
       <div className="BillItem">
-        <input type="text" className="description" value={this.props.description}/>
-        <input type="number" className="price" value={this.props.price}/>
+        <input type="text" className="description" onChange={this.descriptionChange} value={this.props.description}/>
+        <input type="number" className="price" onChange={this.priceChange} value={this.props.price}/>
         <button onClick={(event)=>this.props.deleteMe(event, this.props.index)}>Delete</button>
       </div>
     );
