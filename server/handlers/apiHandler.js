90bd188cc6f34aae29a76a634cfcde248ba233ae
David Walsh <dawalsh@gmail.com>
2016-10-23 10:48:52 -0700

    Database work to complete bill creation (#27)
    * Create server file
    
    * Add hashids and supertest
    
    * Ignore databases
    
    * Create controllers for database models
    
    * Implement short ids and create join table for debtors
    
    * Implement api handler specs
    
    * Implement save bill
    
    * Update README

--- server/handlers/apiHandler.js
+++ server/handlers/apiHandler.js
@@ -1,5 +1,24 @@
-const saveBill = (request, response) => {
+const billController = require('../dbControllers/billController');
 
+const saveBill = (request, response) => {
+  const bill = request.body;
+  billController.createBill(bill)
+    .then(bill => {
+      response.status(201);
+      response.json({
+        data: {
+          shortId: bill.shortId,
+        },
+      });
+    })
+    .catch(error => {
+      response.status(400);
+      response.json({
+        error: {
+          message: error.message,
+        },
+      });
+    });
 
 };
 
