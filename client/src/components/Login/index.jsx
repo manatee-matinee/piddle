ea10cc60f5978dd5c0547250fc6f3e3f51971830
Luis Villeda <luisevilleda@gmail.com>
2016-10-25 14:06:29 -0700

    Complete login signup form with api calls

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< 31303eeaafc363516b0450d58e3d73635499596b
 <<<<<<< 4301587211c154a3a3761875b81eca8b440e224d
 import React, { Component } from 'react';
 import Request from '../../utils/requestHandler';
@@ -75,19 +76,78 @@ class Login extends Component {
 }
 =======
 import React from 'react';
+=======
+import React, {Component} from 'react';
+import Request from '../../utils/requestHandler';
+>>>>>>> Complete login signup form with api calls
 
-const Login = () => (
-  <div className="Login Page">
-    <p className="Login-intro">
-      Welcome to the login page
-    </p>
-    <form id="loginForm">
-      <label for="email">email</label>
-      <input
-        type="text"
-        className="email"
-        name="email"
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.submitLoginForm = this.submitLoginForm.bind(this);
+    this.submitSignupForm = this.submitSignupForm.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  submitLoginForm(event) {
+    event.preventDefault();
+    var email = this.state.email;
+    var password = this.state.password;
+    Request.postLogin(email, password, res => console.log(res));
+  }
+
+  submitSignupForm(event) {
+    event.preventDefault();
+    var email = this.state.email;
+    var password = this.state.password;
+    Request.postSignup(email, password, res => console.log(res));
+  }
+
+  render() {
+    return (
+    <div className="Login Page">
+      <p className="Login-intro">
+        Welcome to the login page
+      </p>
+      <form id="loginForm">
+        <label for="email">email</label>
+        <input
+          type="text"
+          className="email"
+          name="email"
+          onChange={event => this.handleEmailChange(event)}
+          />
+        <label for="password">password</label>
+        <input
+          type="password"
+          className="password"
+          name="password"
+          onChange={event => this.handlePasswordChange(event)}
         />
+        <input
+        type="submit"
+        value="Login"
+        onClick={event => this.submitLoginForm(event)}
+        />
+        <input
+        type="submit"
+        value="Signup"
+        onClick={event => this.submitSignupForm(event)}
+        />
+<<<<<<< 31303eeaafc363516b0450d58e3d73635499596b
       <label for="password">password</label>
       <input
         type="text"
@@ -102,5 +162,12 @@ const Login = () => (
   </div>
 );
 >>>>>>> Create Login page and css file
+=======
+      </form>
+    </div>
+  );
+  }
+}
+>>>>>>> Complete login signup form with api calls
 
 export default Login;
