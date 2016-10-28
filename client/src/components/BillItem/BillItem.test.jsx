d006ad8a4318bf4031d7a85699ff39467a43ad5d
cashweaver <cashbweaver@gmail.com>
2016-10-27 22:42:40 -0700

    Add 'uneditable' tests for BillItem

--- client/src/components/BillItem/BillItem.test.jsx
+++ client/src/components/BillItem/BillItem.test.jsx
@@ -7,46 +7,90 @@ import BillItem from './index';
 const changeBillItem = () => {};
 const deleteBillItem = () => {};
 
-const shallowBillItemProps = {
-  changeBillItem,
-  deleteBillItem,
-  description: 'A wonderful BillItem',
-  index: 14,
-  price: 12.34,
+
+const props = {
+  shallow: {
+    changeBillItem,
+    deleteBillItem,
+    description: 'A wonderful BillItem',
+    index: 14,
+    price: 12.34,
+  },
 };
-const shallowBillItem = shallow(
-  <BillItem {...shallowBillItemProps} />
+
+const selectors = {
+  description: `input[name="billItem-${props.shallow.index}-description"]`,
+  price: `input[name="billItem-${props.shallow.index}-price"]`,
+};
+
+const shallowBillItem =
+  shallow(
+  <BillItem {...props.shallow} />
 );
 
+const renderedComponent = {
+  shallow: {
+    editable: shallow(
+      <BillItem {...props.shallow} />
+    ),
+    uneditable: shallow(
+      <BillItem
+        {...props.shallow}
+        isEditable={false}
+      />
+    ),
+  },
+};
+
 it('renders without crashing', () => {
-  // eslint-disable-next-line no-undef
-  const div = document.createElement('div');
   ReactDOM.render(
-    <BillItem
-      changeBillItem={changeBillItem}
-      deleteBillItem={deleteBillItem}
-      description="A wonderful BillItem"
-      index={14}
-      price={12.34}
-    />,
-    div
+    <BillItem {...props.shallow} />,
+    // eslint-disable-next-line no-undef
+    document.createElement('div'),
   );
 });
 
-it('has a field for inputting the description', () => {
-  expect(shallowBillItem.find('input.description')).to.have.length(1);
+describe('editable', () => {
+  const component = renderedComponent.shallow.editable;
+
+  it('has a field for inputting the description', () => {
+    expect(component.find(selectors.description))
+      .to.have.length(1);
+  });
+
+  it('has a field for inputting the price', () => {
+    expect(component.find(selectors.price))
+      .to.have.length(1);
+  });
+
+  it('populates the appropriate description', () => {
+    expect(component.find(selectors.description).props().value)
+      .to.equal(props.shallow.description);
+  });
+
+  it('populates the appropriate price', () => {
+    expect(component.find(selectors.price).props().value)
+      .to.equal(props.shallow.price);
+  });
 });
 
-it('has a field for inputting the price', () => {
-  expect(shallowBillItem.find('input.price')).to.have.length(1);
-});
+describe('uneditable', () => {
+  const component = renderedComponent.shallow.uneditable;
 
-it('populates the appropriate description', () => {
-  expect(shallowBillItem.find('input.description').props().value)
-    .to.equal(shallowBillItemProps.description);
-});
+  it('should not have input fields', () => {
+    expect(component.find('input'))
+      .to.have.length(0);
+  });
 
-it('populates the appropriate price', () => {
-  expect(shallowBillItem.find('input.price').props(). alue)
-    .to.equal(shallowBillItemProps.price);
+  it('should display the description', () => {
+    // eslint-disable-next-line no-unused-expressions
+    expect(component.html().includes(props.shallow.description))
+      .to.be.true;
+  });
+
+  it('should display the price', () => {
+    // eslint-disable-next-line no-unused-expressions
+    expect(component.html().includes(props.shallow.price))
+      .to.be.true;
+  });
 });
