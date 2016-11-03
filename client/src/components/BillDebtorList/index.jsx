6e3b0810ed20989aba4b91fd740dd96de6abbb58
Marissa <marissaokada@gmail.com>
2016-11-02 18:55:43 -0700

    Add additional information to debtor list

--- client/src/components/BillDebtorList/index.jsx
+++ client/src/components/BillDebtorList/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
+/**
+ * @class BillDebtorList
+ * @param {object} props
+ * @param {object[]} props.items
+ * @param {function} props.deleteBillItem
+ * @param {function} props.changeBillItem
+ * @param {symbol} props.interactionType
+ * @param {function} props.newBillItem
+ */
+
 const BillDebtorList = (props) => {
   return(
 
