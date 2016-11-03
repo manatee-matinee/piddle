fef5f284b283b7f259f51f0ad958b8e4bc8bb9a2
Marissa <marissaokada@gmail.com>
2016-11-02 19:10:56 -0700

    Write part of map function that will render new bill debtors

--- client/src/components/BillDebtorList/index.jsx
+++ client/src/components/BillDebtorList/index.jsx
@@ -4,21 +4,29 @@ import { Button } from 'react-bootstrap';
 /**
  * @class BillDebtorList
  * @param {object} props
- * @param {object[]} props.items
- * @param {function} props.deleteBillItem
- * @param {function} props.changeBillItem
- * @param {symbol} props.interactionType
- * @param {function} props.newBillItem
+ * @param {object[]} props.debtors
+ * @param {function} props.newBillDebtor
  */
 
 const BillDebtorList = (props) => {
   return(
-
+    <div className="BillDebtorList">
+      {props.debtors.map((debtor, i) => (
+        <BillDebtor
+        key={i}
+        index={i}
+        {...debtor}
+        />
+      ))}
+    </div>
   );
 };
 
 BillDebtorList.propTypes = {
-
+  debtors: React.PropTypes.arrayOf(
+    React.PropTypes.object
+  ).isRequired,
+  newBillDebtor: React.PropTypes.func.isRequired,
 };
 
 export default BillDebtorList;
