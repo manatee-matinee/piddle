331d1ab6e5024b12099c49c6045e1dbcdbbb52a0
Marissa <marissaokada@gmail.com>
2016-11-03 11:00:57 -0700

    Finish structure of BillDebtor component

--- client/src/components/BillDebtor/index.jsx
+++ client/src/components/BillDebtor/index.jsx
@@ -1,3 +1,46 @@
 import React from 'react';
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
 
+const BillDebtor = (props) => {
+  const fieldChange = (event) => {
+    const field = {
+      name: event.target.getAttribute('name').match(/([a-z]+)$/)[1],
+      tagName: event.target.tagName,
+      type: event.target.getAttribute('type'),
+    };
+  }
+
+  return (
+    <div className="BillDebtor">
+      <div>
+        <FormControl
+          className="debtor"
+          name={`billDebtor-${props.index}-description`}
+          onChange={fieldChange}
+          placeholder="Debtor"
+          type="text"
+          value={props.debtor}
+          disabled={!!props.payerId}
+        />
+        <FormGroup>
+          <InputGroup>
+            <InputGroup.Button>
+              <Button
+                bsStyle="danger"
+                onClick={event => props.deleteBillDebtor(event, props.index)}
+                disabled={!!props.payerId}>
+                Delete
+              </Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
+      </div>
+    </div>
+  );
+};
+
+BillDebtor.propTypes = {
+  deleteBillDebtor: React.PropTypes.func.isRequired,
+  payerId: React.PropTypes.string.isRequired,
+  debtor: React.PropTypes.string.isRequired
+}
